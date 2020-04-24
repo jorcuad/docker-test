146b38f60b54d4bdb846a4c24bcc43b64f0232de
Croke <jorgecuadradosaez@gmail.com>
2020-04-25 00:07:36 +0200

    first api endpoints for create user, room, join room

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import time
 import sqlite3
 import datetime
+import secrets
 
-from flask import g, Flask
+from flask import g, Flask, Response, abort, request
 app = Flask(__name__)
 
 DATABASE = './bd/wardrums.db'
 DATABASE_NAME ='wardrums'
 DATABASE_SCHEMA ='./bd/wardrums.sql'
+VERSION = '/v1'
+API = '/api'
+USERS_API = API + VERSION + '/users'
+ROOMS_API = API + VERSION + '/rooms'
 
 def get_db():
     db = getattr(g, DATABASE_NAME, None)
@@ -54,12 +59,11 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-
-@app.route('/status')
+@app.route('/status', methods=['GET'])
 def hello():
     return 'I am alive.\n'
 
-@app.route('/connect')
+@app.route('/connect', methods=['GET'])
 def connect():
     try:
         db = get_db()
@@ -67,6 +71,52 @@ def connect():
     except Exception as e:
         print(e)
         return "Error on connection"
+
+@app.route(USERS_API+'/create', methods=['POST'])
+def create_user():
+    try:
+        if not request.json or not request.is_json:
+            abort(400, description="Bad request.")
+        data = request.get_json()
+        user = (secrets.randbelow(999999), data['name'], datetime.datetime.now(), data['mail'], data['password'], data['birthday'])
+        db = get_db()
+        db.cursor().execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)", user)
+        db.commit()
+        return Response(status=201)
+    except Exception as e:
+        abort(500, description="Error creating user.")
+
+@app.route(USERS_API+'/update', methods=['POST'])
+def update_user():
+    return "Not implemented yet."
+
+@app.route(ROOMS_API+'/create', methods=['POST'])
+def create_room():
+    try:
+        if not request.json or not request.is_json:
+            abort(400, description="Bad request.")
+        data = request.get_json()
+        room = (secrets.randbelow(999999), data['capacity'], data['duration'] )
+        db = get_db()
+        db.cursor().execute("INSERT INTO rooms VALUES(?, ?, ?)", room)
+        db.commit()
+        return Response(status=201)
+    except Exception as e:
+        abort(500, description="Error creating room.")
+
+@app.route(ROOMS_API+'/join', methods=['POST'])
+def join_room():
+    try:
+        if not request.json or not request.is_json:
+            abort(400, description="Bad request.")
+        data = request.get_json()
+        join = (data['user_id'], data['room_id'])
+        db = get_db()
+        db.cursor().execute("INSERT INTO user_rooms VALUES(?, ?)", join)
+        db.commit()
+        return Response(status=201)
+    except Exception as e:
+        abort(500, description="Error joining room.")
 
 @app.route('/')
 def index():
